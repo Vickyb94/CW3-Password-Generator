bfe3ce9a1fd8aee15a49707a2f1a9f807e470fcc
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2022-11-09 12:18:50 -0400

    created array of numbers,symbols,lowercase and uppercase characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,26 @@
-// Assignment code here
-
-function generatePassword() {
-  /*created a notification to know when password button is clicked*/
-  console.log('Password Button!')
-}
-
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+//Array of characters 
+var includeUppercaselist;
+var includeLowercaselist;
+var includeNumberlist;
+var includeSymbollist;
+
+var uppercaselist = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberlist = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbollist = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
